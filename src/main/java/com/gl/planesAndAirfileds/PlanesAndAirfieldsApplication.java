62be3cef8c6c9fa89b39c19b54408a928a9a325b
marcin.majka <marcin.majka@gloabllogic.com>
2017-02-14 10:35:50 +0100

    Push test

--- src/main/java/com/gl/planesAndAirfileds/PlanesAndAirfieldsApplication.java
+++ src/main/java/com/gl/planesAndAirfileds/PlanesAndAirfieldsApplication.java
@@ -3,6 +3,9 @@ package com.gl.planesAndAirfileds;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Push TEST
+ */
 @SpringBootApplication
 public class PlanesAndAirfieldsApplication {
 
