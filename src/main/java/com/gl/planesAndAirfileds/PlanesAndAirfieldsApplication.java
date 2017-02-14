68b1cdf69d313e29da7f1e6fc909054ef85ef0e4
jacek.cygan <jacek.cygan@globallogic.com>
2017-02-14 10:02:46 +0100

    skeleton project

---
+++ src/main/java/com/gl/planesAndAirfileds/PlanesAndAirfieldsApplication.java
@@ -0,0 +1,12 @@
+package com.gl.planesAndAirfileds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanesAndAirfieldsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanesAndAirfieldsApplication.class, args);
+	}
+}
