8b738c2bc4bcd6cef69c67849c7ee7599380955b
marcin.majka <marcin.majka@gloabllogic.com>
2017-02-15 13:06:32 +0100

    thymeleaf integration

---
+++ src/main/java/com/gl/planesAndAirfileds/controller/PlanesController.java
@@ -0,0 +1,7 @@
+package com.gl.planesAndAirfileds.controller;
+
+/**
+ * Created by marcin.majka on 15/2/2017.
+ */
+public class PlanesController {
+}
---
+++ src/main/java/com/gl/planesAndAirfileds/controller/ViewController.java
@@ -0,0 +1,17 @@
+package com.gl.planesAndAirfileds.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by marcin.majka on 15/2/2017.
+ */
+
+@Controller
+public class ViewController {
+
+    @RequestMapping("/")
+    public String displayMap() {
+        return "index";
+    }
+}
