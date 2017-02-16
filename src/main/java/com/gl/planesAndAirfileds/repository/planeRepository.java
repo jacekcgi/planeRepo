131bdb5fb85c92565594068939ebcad6a05fb0ec
jacek.cygan <jacek.cygan@globallogic.com>
2017-02-16 08:42:42 +0100

    CredentialsTest

---
+++ src/main/java/com/gl/planesAndAirfileds/repository/planeRepository.java
@@ -0,0 +1,7 @@
+package com.gl.planesAndAirfileds.repository;
+
+import com.gl.planesAndAirfileds.domain.Plane;
+import org.springframework.data.repository.CrudRepository;
+
+public interface planeRepository extends CrudRepository<Plane,Long> {
+}
