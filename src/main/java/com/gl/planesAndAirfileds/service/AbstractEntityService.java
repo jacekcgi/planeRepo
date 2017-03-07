52c6bc7f9a6de0ceb2a8f120a3cb1373466a1cb9
marek.sroga <marek.sroga@globallogic.com>
2017-03-07 08:00:37 +0100

    services methods from crud

--- src/main/java/com/gl/planesAndAirfileds/service/AbstractEntityService.java
+++ src/main/java/com/gl/planesAndAirfileds/service/AbstractEntityService.java
@@ -7,5 +7,27 @@ import java.io.Serializable;
 /**
  * Created by marek.sroga on 2017-03-06.
  */
-public interface AbstractEntityService<T extends AbstractEntity, S extends Serializable> {
+public interface AbstractEntityService<T extends AbstractEntity, ID extends Serializable> {
+
+    <S extends T> S save(S entity);
+
+    <S extends T> Iterable<S> save(Iterable<S> entities);
+
+    T findOne(ID id);
+
+    boolean exists(ID id);
+
+    Iterable<T> findAll();
+
+    Iterable<T> findAll(Iterable<ID> ids);
+
+    long count();
+
+    void delete(ID id);
+
+    void delete(T entity);
+
+    void delete(Iterable<? extends T> entities);
+
+    void deleteAll();
 }
