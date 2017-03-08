ebbfde740e2f0f28293c79cddafb36b2c0f6ccff
krzysztof.gonia <krzysztof.gonia@globallogic.com>
2017-03-08 11:15:48 +0100

    sql update

--- src/main/resources/db/0/db-schema-0.sql
+++ src/main/resources/db/0/db-schema-0.sql
@@ -18,19 +18,23 @@ CREATE INDEX plane_create_date_index ON plane (create_date) USING BTREE;
 CREATE INDEX plane_registration_index ON plane (registration) USING BTREE;
 CREATE INDEX plane_update_date_index ON plane (update_date) USING BTREE;
 
-CREATE TABLE flight_details (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
-  average_fuel_consumption int(11) DEFAULT NULL,
-  course double DEFAULT NULL,
-  gps_latitude double DEFAULT NULL,
-  gps_longitude double DEFAULT NULL,
-  incoming_time datetime DEFAULT NULL,
-  is_actual_position bit(1) DEFAULT NULL,
-  velocity float DEFAULT NULL,
-  plane_id bigint(20) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (plane_id) REFERENCES plane (id) ON DELETE CASCADE
+CREATE TABLE `flight_details` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `average_fuel_consumption` int(11) DEFAULT NULL,
+  `course` double DEFAULT NULL,
+  `distance_traveled` double DEFAULT NULL,
+  `flight_distance` double DEFAULT NULL,
+  `flight_time` bigint(20) DEFAULT NULL,
+  `gps_latitude` double DEFAULT NULL,
+  `gps_longitude` double DEFAULT NULL,
+  `incoming_time` datetime DEFAULT NULL,
+  `is_actual_position` bit(1) DEFAULT NULL,
+  `remaining_fuel` double DEFAULT NULL,
+  `velocity` float DEFAULT NULL,
+  `plane_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
 CREATE INDEX flight_details_incoming_time_index ON flight_details (incoming_time) USING BTREE;
 
 COMMIT;
