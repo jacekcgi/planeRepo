806be63d4524f079ce0d773a25d7c7d69b43e3cd
marek.sobieraj <mareczek18>
2017-03-09 12:51:43 +0100

    merged

--- src/main/resources/static/src/main.ts
+++ src/main/resources/static/src/main.ts
@@ -4,13 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
 
+declare var module: any;
 if (process.env.ENV === 'production') {
   enableProdMode();
-}
-// Enables Hot Module Replacement.
-declare var module: any;
-if (module.hot) {
-    module.hot.accept();
+} else if (process.env.ENV === 'development') {
+  // Enables Hot Module Replacement.
+  if (module.hot) {
+      module.hot.accept();
+  }
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
