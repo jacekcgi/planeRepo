2930f41d7199da459e4e2b533010247cd04a4b21
krzysztof.gonia <krzysztof.gonia@globallogic.com>
2017-03-21 11:48:15 +0100

    removed unnused methods

--- src/main/java/com/gl/planesAndAirfileds/controller/ViewController.java
+++ src/main/java/com/gl/planesAndAirfileds/controller/ViewController.java
@@ -1,20 +1,11 @@
 package com.gl.planesAndAirfileds.controller;
 
-import com.gl.planesAndAirfileds.service.PlaneService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ViewController {
-
-    private PlaneService planeService;
-
-    @Autowired
-    public ViewController(PlaneService planeService) {
-        this.planeService = planeService;
-    }
 
     @RequestMapping(value =
             {
