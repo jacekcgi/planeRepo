fcee735dbc48f8108bb0a7ca7b83bbcfaab970ac
krzysztof.gonia <krzysztof.gonia@globallogic.com>
2017-03-22 12:25:17 +0100

    NOK-68 Translations, GUI, cookie configuration

---
+++ src/main/resources/static/src/app/translate/en_EN/airplane.ts
@@ -0,0 +1,8 @@
+export const airplane = {
+  title: 'Planes',
+  name: 'Name',
+  registration: 'Registration',
+  description: 'Description',
+  successCreated: 'Plane has been created',
+  formHeader: 'Add Plane'
+}
---
+++ src/main/resources/static/src/app/translate/pl_PL/airplane.ts
@@ -0,0 +1,9 @@
+export const airplane = {
+  title: 'Samoloty',
+  name: 'Nazwa',
+  registration: 'Numer rejestracyjny',
+  description: 'Opis',
+  successCreated: 'Samolot został stworzony',
+  formHeader: 'Dodaj samolot'
+  
+}
