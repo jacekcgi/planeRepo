366159fb07303c04f5637e9c824bcd4da3317a91
marek.sobieraj <mareczek18>
2017-03-22 11:24:38 +0100

    crud repository removed ; ourself CRUD methods rewrites + tests

---
+++ src/test/java/com/gl/planesAndAirfileds/repository/impl/CustomNestedEntityRepository.java
@@ -0,0 +1,12 @@
+package com.gl.planesAndAirfileds.repository.impl;
+
+import com.gl.planesAndAirfileds.domain.NestedEntity;
+import com.gl.planesAndAirfileds.repository.CustomAbstractEntityRepository;
+
+/**
+ * Created by krzysztof.gonia on 3/8/2017.
+ * <p>
+ * Additional repository for tests
+ */
+public interface CustomNestedEntityRepository extends CustomAbstractEntityRepository<NestedEntity> {
+}
---
+++ src/test/java/com/gl/planesAndAirfileds/repository/impl/NestedEntityRepository.java
@@ -0,0 +1,15 @@
+package com.gl.planesAndAirfileds.repository.impl;
+
+import com.gl.planesAndAirfileds.domain.NestedEntity;
+import com.gl.planesAndAirfileds.repository.AbstractEntityRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Additional interface for tests
+ */
+
+@Repository
+public interface NestedEntityRepository
+        extends AbstractEntityRepository<NestedEntity, Long>, CustomNestedEntityRepository {
+
+}
