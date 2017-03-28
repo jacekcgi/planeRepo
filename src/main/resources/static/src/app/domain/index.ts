c2c7025e52db03dd12b3b70c2ce2977f1f0feb5e
krzysztof.gonia <krzysztof.gonia@globallogic.com>
2017-03-28 09:10:26 +0200

    missing file

---
+++ src/main/resources/static/src/app/domain/index.ts
@@ -0,0 +1,14 @@
+
+export interface FlightDetailsDto {
+    currentLatitude: number;
+
+    currentLongitude: number;
+
+    destinationLatitude: number;
+
+    destinationLongitude: number;
+
+    velocity: number;
+
+    flightRouteSid: string;
+}
