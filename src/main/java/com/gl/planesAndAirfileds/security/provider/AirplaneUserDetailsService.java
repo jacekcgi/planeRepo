281cd427a31659e65c750c1c40eb0b0c3e38554d
marek.sobieraj <mareczek18>
2017-03-31 14:06:46 +0200

    jacek todo

--- src/main/java/com/gl/planesAndAirfileds/security/provider/AirplaneUserDetailsService.java
+++ src/main/java/com/gl/planesAndAirfileds/security/provider/AirplaneUserDetailsService.java
@@ -16,7 +16,7 @@ public class AirplaneUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
 
-        // TODO atm hardcoded by spring password encoder! rawPass = 'password'
+        // TODO JACEK atm hardcoded by spring password encoder! rawPass = 'password'
         String hardcodedPassword = "$2a$05$E957LMo2SWU.vbpL3/AaSul4l1hD4OdyQmIXslET2.YTAxIc5.76y";
         return new User(s, hardcodedPassword, Collections.emptyList());
     }
